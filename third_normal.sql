41d20c81dcf243e1e99b730d4cefd775e96df7b9
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-25 20:44:12 -0700

    Update third_normal.sql

--- third_normal.sql
+++ third_normal.sql
@@ -3,3 +3,6 @@ CREATE TABLE stats (pokedex_number INTEGER PRIMARY KEY, name TEXT, attack INTEGE
 INSERT INTO stats (pokedex_number, name, attack, speed, hp, sp_attack, sp_defense, defense, experience_growth)
    ...> SELECT attack, speed, hp, sp_attack, sp_defense, defense, experience_growth
    ...> FROM imported_pokemon_data;
+
+
+ALTER TABLE pokemon ADD CONSTRAINT fk_stats_pokemon FOREIGN KEY (pokedex_number) REFERENCES stats(pokedex_number);
