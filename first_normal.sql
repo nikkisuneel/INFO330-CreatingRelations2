6305e575e2dd089369c3ee637522577e1d32e134
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-25 20:54:36 -0700

    Update first_normal.sql

--- first_normal.sql
+++ first_normal.sql
@@ -5,3 +5,12 @@ CREATE TABLE split_abilities AS
    ...> WHERE ability <> '';
 sqlite> 
 
+CREATE TABLE pokemon1 AS
+   ...> SELECT *
+   ...> FROM pokemon
+   ...> INNER JOIN split_abilities
+   ...> ON pokemon.pokedex_number = split_abilities.pokedex_number
+   ...> ;
+sqlite> DROP TABLE pokemon;
+ALTER TABLE pokemon1 RENAME TO pokemon;
+
