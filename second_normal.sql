16bc3cd11246f26ae8407b364a60dba9fcf84932
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-25 20:03:38 -0700

    Update second_normal.sql

--- second_normal.sql
+++ second_normal.sql
@@ -1,12 +1,12 @@
 
 CREATING/INSERTING INTO TABLES
 
-CREATE TABLE pokemon (pokedex_number INTEGER PRIMARY KEY, name TEXT, classification TEXT, type1 TEXT, type2 TEXT, height_m FLOAT, weight_kg FLOAT, base_happiness INTEGER, capture_rate INTEGER, base_egg_steps INTEGER, abilities TEXT, percentage_male FLOAT, generation INTEGER, is_legendary INTEGER);
+CREATE TABLE pokemon (pokedex_number INTEGER PRIMARY KEY, name TEXT, classification TEXT, type1 TEXT, type2 TEXT, height_m FLOAT, weight_kg FLOAT, base_happiness INTEGER, capture_rate INTEGER, base_egg_steps INTEGER, ability TEXT, percentage_male FLOAT, generation INTEGER, is_legendary INTEGER);
 CREATE TABLE stats (pokedex_number INTEGER PRIMARY KEY, name TEXT, attack INTEGER, speed INTEGER, hp INTEGER, sp_attack INTEGER, sp_defense INTEGER, defense INTEGER, experience_growth INTEGER);
 CREATE TABLE against (pokedex_number INTEGER PRIMARY KEY, name TEXT, against_bug NUMERIC, against_dark NUMERIC, against_dragon NUMERIC, against_electric NUMERIC, against_fairy NUMERIC, against_fight NUMERIC, against_fire NUMERIC, against_flying NUMERIC, against_ghost NUMERIC, against_grass NUMERIC, against_ground NUMERIC, against_ice NUMERIC, against_normal NUMERIC, against_poison NUMERIC, against_psychic NUMERIC, against_rock NUMERIC, against_steel NUMERIC, against_water NUMERIC);
 
-INSERT INTO pokemon (pokedex_number, name, classification, type1, type2, height_m, weight_kg, capture_rate, base_egg_steps, abilities, percentage_male, base_happiness, is_legendary, generation)
-   ...> SELECT pokedex_number, name, classfication, type1, type2, height_m, weight_kg, capture_rate, base_egg_steps, abilities, percentage_male, base_happiness, is_legendary, generation
+INSERT INTO pokemon (pokedex_number, name, classification, type1, type2, height_m, weight_kg, capture_rate, base_egg_steps, ability, percentage_male, base_happiness, is_legendary, generation)
+   ...> SELECT pokedex_number, name, classfication, type1, type2, height_m, weight_kg, capture_rate, base_egg_steps, ability, percentage_male, base_happiness, is_legendary, generation
    ...> FROM imported_pokemon_data;
    
 INSERT INTO stats (pokedex_number, name, attack, speed, hp, sp_attack, sp_defense, defense, experience_growth)
