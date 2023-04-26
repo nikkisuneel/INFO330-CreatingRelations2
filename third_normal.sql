d29413659181185cf0b1e89091d0ed981f1916c7
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-25 20:06:12 -0700

    Update third_normal.sql

--- third_normal.sql
+++ third_normal.sql
@@ -1,4 +1,4 @@
-
+CREATE TABLE stats (pokedex_number INTEGER PRIMARY KEY, name TEXT, attack INTEGER, speed INTEGER, hp INTEGER, sp_attack INTEGER, sp_defense INTEGER, defense INTEGER, experience_growth INTEGER);
 
 INSERT INTO stats (pokedex_number, name, attack, speed, hp, sp_attack, sp_defense, defense, experience_growth)
    ...> SELECT attack, speed, hp, sp_attack, sp_defense, defense, experience_growth
