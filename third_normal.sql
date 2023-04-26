db4bfd943c100d3f48a54a88740c6936cf180d17
nikkisuneel <53950214+nikkisuneel@users.noreply.github.com>
2023-04-25 20:05:37 -0700

    Update third_normal.sql

--- third_normal.sql
+++ third_normal.sql
@@ -1,1 +1,5 @@
 
+
+INSERT INTO stats (pokedex_number, name, attack, speed, hp, sp_attack, sp_defense, defense, experience_growth)
+   ...> SELECT attack, speed, hp, sp_attack, sp_defense, defense, experience_growth
+   ...> FROM imported_pokemon_data;
